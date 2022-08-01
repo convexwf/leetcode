27ef81c0328b35ac3ac9136e94ded95c1abb0904
convexwf <convexwf@gmail.com>
2022-08-01 22:00:01 +0800

    HP-Win10 2022-08-01 Commit

--- .code/9.palindrome-number.cpp
+++ .code/9.palindrome-number.cpp
@@ -14,57 +14,91 @@
  * Testcase Example:  '121'
  *
  * Given an integer x, return true if x is palindrome integer.
- * 
+ *
  * An integer is a palindrome when it reads the same backward as forward.
- * 
- * 
+ *
+ *
  * For example, 121 is a palindrome while 123 is not.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: x = 121
  * Output: true
  * Explanation: 121 reads as 121 from left to right and from right to left.
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: x = -121
  * Output: false
  * Explanation: From left to right, it reads -121. From right to left, it
  * becomes 121-. Therefore it is not a palindrome.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: x = 10
  * Output: false
  * Explanation: Reads 01 from right to left. Therefore it is not a
  * palindrome.
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * -2^31 <= x <= 2^31 - 1
- * 
- * 
- * 
+ *
+ *
+ *
  * Follow up: Could you solve it without converting the integer to a string?
  */
 
 // @lc code=start
+// 2022-08-01 submission
+// 11510/11510 cases passed
+// Runtime: 22 ms, faster than 54.64% of C++ online submissions.
+// Memory Usage: 5.9 MB, less than 31.85% of C++ online submissions.
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        if (x < 0) return false;
+        int div = 1;
+        while (x / div >= 10) div *= 10;
+        while (x > 0) {
+            int left = x / div;
+            int right = x % 10;
+            if (left != right) return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
     }
 };
 // @lc code=end
 
+// @lc code=start
+// 2022-08-01 submission
+// 11510/11510 cases passed
+// Runtime: 15 ms, faster than 81.79% of C++ online submissions.
+// Memory Usage: 5.9 MB, less than 31.85% of C++ online submissions.
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int reverted = 0;
+        while (x > reverted) {
+            reverted = reverted * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reverted || x == reverted / 10;
+    }
+};
+// @lc code=end
