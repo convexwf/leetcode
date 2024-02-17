04b7c99b2d9cba1a0241ba3e1e724cf06e14bf2c
convexwf <convexwf@gmail.com>
2024-02-18 06:00:13 +0800

    TencentCloud 2024-02-18 Commit

---
+++ .cpp/798.smallest-rotation-with-highest-score.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=798 lang=cpp
+ *
+ * [798] Smallest Rotation with Highest Score
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int bestRotation(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
