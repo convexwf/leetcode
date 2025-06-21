a033069b60d5072f5ff593ff7334b55899f5d53c
convexwf <convexwf@gmail.com>
2025-06-22 06:00:12 +0800

    TencentCloud 2025-06-22 Commit

--- .cpp/0156.binary-tree-upside-down.cpp
+++ .cpp/0156.binary-tree-upside-down.cpp
@@ -5,5 +5,20 @@
  */
 
 // @lc code=start
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root || !root->left) {
+            return root;
+        }
 
+        TreeNode* newRoot = upsideDownBinaryTree(root->left);
+        root->left->right = root;       // 原根节点变成新根节点的右孩子
+        root->left->left = root->right; // 原右孩子变成新根节点的左孩子
+        root->left = nullptr;           // 清空原左孩子
+        root->right = nullptr;          // 清空原右孩子
+
+        return newRoot;
+    }
+};
 // @lc code=end
